ae2fbfcd9f997152d1d0ff6a2a6c25a8b0023ba7
credo-negi <korekiyo1985@gmail.com>
2025-02-07 00:59:38 +0900

    GitHub Actionsでのビルドに備えた設定

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,19 @@ import react from '@vitejs/plugin-react-swc'
 import styleX from 'vite-plugin-stylex'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), styleX()],
-  // build options
-  build: {
-    // dist フォルダに出力
-    outDir: 'dist',
-    // dist フォルダが存在しない場合は作成
-    emptyOutDir: true,
+export default defineConfig(() => {
+  // GitHub Actions でビルドされた場合は、リポジトリ名を取得
+  const isGitHubActions = process.env.GITHUB_ACTIONS === 'true'
+  const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+  return {
+    plugins: [react(), styleX()],
+    // build options
+    build: {
+      // dist フォルダに出力
+      outDir: 'dist',
+      // dist フォルダが存在しない場合は作成
+      emptyOutDir: true,
+    },
+    base: isGitHubActions ? `/${repoName}/` : '/',
   }
 })
